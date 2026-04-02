a5616d99543112a972bbaed4c21ed2bf9cf8001d
Daniil Dolmatov <dandolmatov@10.215.198.143-vpn.dhcp.yndx.net>
2026-04-02 19:19:50 +0300

    new syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -112,7 +112,45 @@ uint64
 sys_pte(void)
 {
   struct proc *p = myproc();
-  pagetable_t pt = p->pagetable;
-  vmprint(pt, 0);
+  vmprint(p->pagetable, 0);
+  return 0;
+}
+
+uint64
+sys_change_flag(void)
+{
+  uint64 buf, len, mask;
+  pagetable_t pt = myproc()->pagetable;
+  argaddr(0, &buf);
+  argaddr(1, &len);
+  argint(2, (int*)&mask);
+
+  if (mask >> 8) {
+    return -1;
+  }
+
+  if (mask & ~(PTE_D | PTE_A)) {
+    return -1;
+  }
+
+  uint64 va, pa;
+  int n;
+
+  while (len > 0) {
+    va = PGROUNDDOWN(buf);
+    pte_t *pte = walk(pt, va, 0);
+    if (pte == 0 || !(*pte & PTE_V) || !(*pte & PTE_U))
+      return -1;
+    pa = PTE2PA(*pte);
+    if (pa == 0)
+      return -1;
+    n = PGSIZE - (buf - va);
+    if (n > len) n = len;
+    buf += n;
+    len -= n;
+    *pte &= ~mask;
+  }
+  sfence_vma();
+
   return 0;
 }
--- user/test.c
+++ user/test.c
@@ -5,6 +5,7 @@
 int
 main(int argc, char *argv[])
 {
+  // test pte syscall
   pte();
   exit(0);
 }
